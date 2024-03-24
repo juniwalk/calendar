b76ca4a8531631fb7904e5ac1a6021237c22cefe
juniwalk <juniwalk@outlook.cz>
2024-03-24 22:49:12 +0100

    Added basic DI extension

---
+++ src/DI/CalendarExtension.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+/**
+ * @copyright Martin Procházka (c) 2024
+ * @license   MIT License
+ */
+
+namespace JuniWalk\Calendar\DI;
+
+use JuniWalk\Calendar\CalendarFactory;
+use Nette\DI\CompilerExtension;
+use Nette\Schema\Expect;
+use Nette\Schema\Schema;
+
+final class CalendarExtension extends CompilerExtension
+{
+	public function getConfigSchema(): Schema
+	{
+		return Expect::structure([
+		]);
+	}
+
+
+	/**
+	 * @throws RecordNotValidException
+	 */
+	public function loadConfiguration()
+	{
+		$builder = $this->getContainerBuilder();
+		$config = $this->getConfig();
+
+		// TODO: new Parameters implements Config
+		// TODO: Add values from $config
+		// TODO: Send to instance of Calendar
+
+		$builder->addFactoryDefinition($this->prefix('calendar'))
+			->setImplement(CalendarFactory::class, []);
+	}
+}
