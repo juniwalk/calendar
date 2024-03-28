6d8dae1e7a06ff61e4e5c605824cada1d7aba822
juniwalk <juniwalk@outlook.cz>
2024-03-28 23:00:43 +0100

    Add option to disable editability of specific source

--- src/SourceEditable.php
+++ src/SourceEditable.php
@@ -11,6 +11,8 @@ use DateTimeInterface as DateTime;
 
 interface SourceEditable
 {
+	public function setEditable(bool $isEditable): void;
+
 	public function eventDrop(int $id, DateTime $start, bool $allDay): void;
 	public function eventResize(int $id, DateTime $end, bool $allDay): void;
 }
