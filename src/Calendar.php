718b308a1e77d8229c51cb0e24dbf9c462118be5
juniwalk <juniwalk@outlook.cz>
2024-03-28 15:15:30 +0100

    Rename fetch! params

--- src/Calendar.php
+++ src/Calendar.php
@@ -183,11 +183,6 @@ class Calendar extends Control implements LinkProvider
 
 	public function handleFetch(?string $start, ?string $end, ?string $timeZone): void
 	{
-		// TODO: Try to fetch events with prefixed query parameters
-		$timeZone = $this->httpRequest->getQuery('timeZone');
-		$start = $this->httpRequest->getQuery('start');
-		$end = $this->httpRequest->getQuery('end');
-
 		try {
 			$events = $this->fetchEvents(
 				new DateTime($start),
